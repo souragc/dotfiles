d632baf65da4517d40ba8bedb4435d385afc551f
Aditya Vardhan Padala <padalaadityavardhan@gmail.com>
2023-03-11 16:52:47 -0500

    night

--- init.lua
+++ init.lua
@@ -74,7 +74,7 @@ require('lazy').setup({
     'folke/tokyonight.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'tokyonight'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
