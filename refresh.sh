de9283e805917235c70865f7d6c493b2f3b395ec
Aditya Vardhan Padala <padalaadityavardhan@gmail.com>
2023-03-31 09:49:05 -0400

    add alacritty, fonts, starship

--- refresh.sh
+++ refresh.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cp ./fonts/* ~/.local/share/fonts/
+fc-cache -f -v
+
+cp ./starship.toml ~/.config/starship.toml
 
 cp -r nvim ~/.config/
 cp .tmux.conf ~/.tmux.conf
+cp -r ./alacritty ~/.config/
+
