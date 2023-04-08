c954bd4e7a51bb21b7840a370d6d9d086755dd3e
Aditya Vardhan Padala <padalaadityavardhan@gmail.com>
2023-04-07 23:37:19 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -194,6 +194,10 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 
 -- Keymaps for better default experience
