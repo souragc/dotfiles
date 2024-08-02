072ef837fedbefc65efee2d75c0e12d13169e9eb
Aditya Vardhan Padala <padalaadityavardhan@gmail.com>
2024-08-02 15:07:46 -0400

    s

--- nvim/init.lua
+++ nvim/init.lua
@@ -105,6 +105,14 @@ require("lazy").setup({
       })
     end
   },
+
+  {'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function ()
+      require("bufferline").setup{}
+    end
+  },
 })
 
 -- LSP Configuration
