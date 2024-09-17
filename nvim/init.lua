87e26c375583d3d9e7691091c2de027b5e8dafaa
Sourag Cherupattamoolayil <44165434+souragc@users.noreply.github.com>
2024-09-17 12:12:45 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -115,6 +115,15 @@ require("lazy").setup({
   },
 })
 
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local last_pos = vim.fn.line("'\"")
+    if last_pos > 1 and last_pos <= vim.fn.line("$") then
+      vim.api.nvim_win_set_cursor(0, { last_pos, 0 })
+    end
+  end
+})
 -- LSP Configuration
 
 local servers = {
